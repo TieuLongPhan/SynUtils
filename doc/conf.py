9c14817efa3538bd615ec78271304b54d45047cf
TieuLongPhan <ptlong.chcndpbc20@ump.edu.vn>
2024-10-18 12:36:34 +0200

    fix format

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'synutility'
-copyright = '2024, Tieu-Long Phan'
-author = 'Tieu-Long Phan'
+project = "synutility"
+copyright = "2024, Tieu-Long Phan"
+author = "Tieu-Long Phan"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,13 +19,12 @@ extensions = [
     "sphinx.ext.githubpages",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
